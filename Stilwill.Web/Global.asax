113671d93f291a33735463f9fdb3c05330791900
Thom Stilwill <thom.stilwill@gmail.com>
2017-02-11 13:50:30 -0500

    Add project files.

---
+++ Stilwill.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Stilwill.Web.MvcApplication" Language="C#" %>
---
+++ Stilwill.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Stilwill.Web.Startup))]
+namespace Stilwill.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
